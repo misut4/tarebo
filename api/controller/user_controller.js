7bad4bdafec2e7fd661a0504f82f605d04409611
mist4kes <sihoang36@gmail.com>
2022-10-16 11:36:40 +0700

    fix getuser by id

--- api/controller/user_controller.js
+++ api/controller/user_controller.js
@@ -1,20 +1,21 @@
 const { ObjectId } = require("mongodb");
 const { default: mongoose } = require("mongoose");
 // const  User  = mongoose.model("User");
-const User = require('../../model/user')
-
+const User = require("../../model/user");
 
 //DEDICATED FUNCTIONS=================================
-async function findOne(req, res, id) {
-    User.find({
-        _id: id
-    })
-    .then((user) => {
-      return res.json(user);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+async function findOne(req, res) {
+  const  id  = req.query.id;
+  console.log(req.query.id);
+  console.log(id);
+  const user = await User.findOne({
+    _id: id
+  });
+  console.log(user);
+  return res.status(200).json({
+    msg: "success",
+    code: 200,
+  });
 }
 
 async function findMany(req, res) {
@@ -28,86 +29,87 @@ async function findMany(req, res) {
 }
 
 async function createOne(req, res) {
-    const _id = req.body._id;
-    const username = req.body.username;
-    const password = req.body.password;
-    const email = req.body.email;
-    const phone = req.body.phone;
-    const role = "user";
-    const isPremium = false;
+  const _id = req.body._id;
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const role = "user";
+  const isPremium = false;
 
-
-    // if (!email || !username || !password) {
-    //     res.status(422).json({ error: "Please add all the fields" })
-    // }
-    //make password not show on database
-    // req.user.password = undefined
-    const user = new User({
-        //key and value are the same so only need to type one
-        _id,
-        username,
-        password,
-        email,
-        phone,
-        role,
-        isPremium
+  // if (!email || !username || !password) {
+  //     res.status(422).json({ error: "Please add all the fields" })
+  // }
+  //make password not show on database
+  // req.user.password = undefined
+  const user = new User({
+    //key and value are the same so only need to type one
+    _id,
+    username,
+    password,
+    email,
+    phone,
+    role,
+    isPremium,
+  });
+  user
+    .save()
+    .then((result) => {
+      return res.json(result);
     })
-    user.save().then(result => {
-       return res.json(result)
-    })
-        .catch(err => {
-            console.log(err)
-        })
-
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 async function updateOne(req, res) {
-    const _id = req.body._id;
-    const username = req.body.username;
-    const password = req.body.password;
-    const email = req.body.email;
-    const phone = req.body.phone
-    const role = "user";
-    const isPremium = false;
+  const _id = req.body._id;
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const role = "user";
+  const isPremium = false;
 
-    if (username ) {
-        res.status(200).json({ msg: "Please add all the fields", code: "400" })
-    }
-    //make password not show on database
-    req.user.password = undefined
-    const user = new User({
-        //key and value are the same so only need to type one
-        _id,
-        username,
-        password,
-        email,
-        phone,
-        role,
-        isPremium
+  if (username) {
+    res.status(200).json({ msg: "Please add all the fields", code: "400" });
+  }
+  //make password not show on database
+  req.user.password = undefined;
+  const user = new User({
+    //key and value are the same so only need to type one
+    _id,
+    username,
+    password,
+    email,
+    phone,
+    role,
+    isPremium,
+  });
+  user
+    .save()
+    .then((result) => {
+      return res.json({ post: result });
     })
-    user.save().then(result => {
-       return res.json({ post: result })
-    })
-        .catch(err => {
-            console.log(err)
-        })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 async function deleteOne(req, res) {
-    try {
-		await User.deleteOne({ _id: req.params.id })
-		res.status(204).send()
-	} catch {
-		res.status(200)
-		res.send({ msg: "User doesn't exist!", code: "400" })
-	}
+  try {
+    await User.deleteOne({ _id: req.params.id });
+    res.status(204).send();
+  } catch {
+    res.status(200);
+    res.send({ msg: "User doesn't exist!", code: "400" });
+  }
 }
 
 //=====================================================================================
 
 //REST API GET=================================================
 const getUser = (req, res) => {
-  const id = req.params;
   findOne(req, res);
 };
 
@@ -137,4 +139,3 @@ module.exports = {
   updateUser,
   deleteUser,
 };
-
