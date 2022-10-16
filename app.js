519f6e09522d721280800f358007a9db97212814
mist4kes <sihoang36@gmail.com>
2022-10-16 09:38:56 +0700

    re

--- app.js
+++ app.js
@@ -68,8 +68,8 @@ app.use(express.urlencoded({ extended: false }));
 //parse json
 app.use(express.json());
 
-// app.use("/", auth);
-// app.use("/", reqLogin);
+app.use("/", auth);
+app.use("/", reqLogin);
 app.use("/user", user);
 app.use("/trip", trip);
 app.use("/plan", plan);
