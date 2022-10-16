423e64eea9fd353c708ba0a28cf2e418dd8974fa
mist4kes <sihoang36@gmail.com>
2022-10-16 09:13:01 +0700

    test1

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -28,12 +28,14 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   //authorization === Bearer "string token"
   if (!authorization) {
-    res.status(200).json({ msg: "you must be logged in 1", code: 400});
+    // res.status(200).json({ msg: "you must be logged in 1", code: 400});
+    res.status(200)
   }
   const token = authorization.replace("Bearer ", "");
   jwt.verify(token, JWT_SECRET, (err, payload) => {
     if (err) {
-      return res.staus(200).json({msg: "you must be logged in", code: 400 });
+      // return res.staus(200).json({msg: "you must be logged in", code: 400 });
+      return res.staus(200)
     }
     const { _id } = payload;
     User.findById(_id).then((userdata) => {
